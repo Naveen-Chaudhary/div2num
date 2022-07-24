d92e1e5d0a71545b8a6b5f2efa7b3365fa86543e
Naveen-Chaudhary <88444252+Naveen-Chaudhary@users.noreply.github.com>
2022-07-24 16:09:43 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ a= st.number_input('Number A')
 
 b =st.number_input('Number B')
 if b == 0:
-  a1=st.write(f'A/B = Undefined)
+  a1=st.write(f'A/B = Undefined')
 else:
   a1=st.write(f'A/B = {a/b}')
 print(a1)
