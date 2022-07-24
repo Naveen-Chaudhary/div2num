0bb79cd3e7fa7f218bcc453689d96bb92b28aeaf
Naveen-Chaudhary <88444252+Naveen-Chaudhary@users.noreply.github.com>
2022-07-24 15:57:32 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,4 +8,4 @@ a= st.number_input('Number A')
 
 b =st.number_input('Number B')
 
-st.write(f'The number we get after dividing number a by number b is {a/b}')
+st.write(f'The number we get after dividing \'Number A' by \'Number B' = {a/b}')
