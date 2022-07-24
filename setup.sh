8655266e1fa034d4a53a80b9e56b00fc3e1b9a5f
Naveen-Chaudhary <88444252+Naveen-Chaudhary@users.noreply.github.com>
2022-07-24 16:16:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo"\
+echo "\
 [general]\n\
 email = \"naveen14062001@gmail.com\"\n\
-" > ~/.streamlit/ceredentials.toml
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
